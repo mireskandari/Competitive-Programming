db78fd514dd63e694fd999d0a5477acddf8f47f8
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2019-11-25 04:50:00 -0500

    Add solution for problem `A - Johny Likes Numbers` Link: https://codeforces.com/contest/678/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/678/submission/65699039

---
+++ codeforces/678/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io  ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define sep      ' '
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define getarr(arr, y) for(int i = 0; i < (y); i++) cin >> (arr)[i]
+#define printarr(arr, y) for(int i = 0; i < (y); i++) cerr << i << " = " << (arr)[i] << endl
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef string str;
+
+
+int main(){
+    fast_io;
+    ll n, k;
+    cin >> n >> k;
+    cout << (n + (k - (n % k)));
+    return 0;
+}
