9c80b1e313ef8ac84222868b9392c6ef9992378e
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2019-11-27 10:07:00 -0500

    Add solution for problem `A - Helpful Maths` Link: https://codeforces.com/contest/339/problem/A Tags: greedy, implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/339/submission/65857292

---
+++ codeforces/339/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io  ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define sep      ' '
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define getarr(arr, y) for(int i = 0; i < (y); i++) cin >> (arr)[i]
+#define printarr(arr, y) for(int i = 0; i < (y)-1; i++) cout << (arr)[i] << '+'
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef string str;
+
+int nums[100];
+int main(){
+    fast_io;
+    str s, part;
+    char deli;
+    cin >> s;
+    deli = '+';
+    int pos, n;
+    pos = 0;
+    n = 0;
+    while((pos = s.find(deli)) != s.npos){
+        part = s.substr(0, pos);
+        nums[n] = stoi(part);
+        s.erase(0, pos + 1);
+        n++;
+    }
+//    cout << s;
+    nums[n] = stoi(s);
+    n++;
+//    cout << n << endl;
+    sort(nums, nums+n);
+    printarr(nums, n);
+    cout << nums[n-1];
+    return 0;
+
+}
