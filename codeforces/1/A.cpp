7fa07fbd70d485b737926dee47413755a78d5cfb
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2019-12-18 14:34:00 -0500

    Add solution for problem `A - Theatre Square` Link: https://codeforces.com/contest/1/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1/submission/67176544

---
+++ codeforces/1/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io          ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define sep              ' '
+#define debug(x)         cerr << #x << " = " << x << endl
+#define getarr(arr, y)   for(int i = 0; i < y; i++) cin >> arr[i]
+#define printarr(arr, y) for(int i = 0; i < y; i++) cerr << i << " = " << arr[i] << endl
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef string str;
+
+
+int main(){
+    fast_io;
+    ll n, m, a;
+    cin >> n >> m >> a;
+    cout << (n / a + bool(n % a)) * (m / a + bool(m % a));
+    return 0;
+}
