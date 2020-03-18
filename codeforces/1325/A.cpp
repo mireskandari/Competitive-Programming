9b91ab8a3a2c622b3fc1da202bc0deb3c6d2dcd8
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-03-18 08:00:00 -0400

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/73566657

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define all(v) v.begin(),v.end()
+#define X first
+#define Y second
+#define forn(x, y, z) for(ll (z) = (x); (z) < (y); (z)++)
+
+void debug_out() { cerr << '\n'; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << H << ' ';
+    debug_out(T...);
+}
+
+int constexpr N = 1e5;
+int constexpr MOD = 1e9 + 7;
+int constexpr INF = 1e9;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+
+        int x;
+        cin >> x;
+        if (x % 2 == 0) {
+            cout << x / 2 << ' ' << x / 2 << '\n';
+        } else {
+            cout << 1 << ' ' << x - 1 << '\n';
+        }
+    }
+    return 0;
+}
