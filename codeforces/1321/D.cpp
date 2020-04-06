2104226adcfc21947566ba3bfc16a89aa240fd12
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-04-06 14:38:00 -0400

    Add solution for problem `D - Navigation System` Link: https://codeforces.com/contest/1321/problem/D Tags: graphs, shortest paths, *1700 Ref: https://codeforces.com/contest/1321/submission/75686792

--- codeforces/1321/D.cpp
+++ codeforces/1321/D.cpp
@@ -11,18 +11,38 @@ int constexpr N = 2 * 1e5 + 10;
 int constexpr MOD = 1e9 + 7;
 ll constexpr INF = 1e13;
 
+int n, m, k;
+vector<int> g[N], pol;
+set<int> p[N];
+bool mk[N];
+ll dist[N];
+
+void bfs(int v) {
+    mk[v] = true;
+    queue<int> q;
+    q.push(v);
+    dist[v] = 0;
+
+    while (!q.empty()) {
+        v = q.front();
+        q.pop();
+        for (auto u : g[v]) {
+            if (!mk[u]) {
+                dist[u] = dist[v] + 1;
+                p[u].insert(v);
+                q.push(u);
+                mk[u] = true;
+            }
+            else if (dist[v] + 1 == dist[u]) {
+                p[u].insert(v);
+            }
+        }
+    }
+}
 int main() {
     ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    int n, m, k;
+
     cin >> n >> m;
-
-    vector<int> pol;
-    auto g = new vector<int>[n];
-    auto p = new set<int>[n];
-    auto mk = new bool[n];
-    auto dist = new ll[n];
-
-    fill(mk, mk + n, false);
     fill(dist, dist + n, INF);
     rep (0, m, i) {
         int u, v;
@@ -35,28 +55,7 @@ int main() {
         cin >> v;
         pol.push_back(--v);
     }
-    function<void(int v)> bfs = [&](int v) {
-        mk[v] = true;
-        queue<int> q;
-        q.push(v);
-        dist[v] = 0;
 
-        while (!q.empty()) {
-            v = q.front();
-            q.pop();
-            for (auto u : g[v]) {
-                if (!mk[u]) {
-                    dist[u] = dist[v] + 1;
-                    p[u].insert(v);
-                    q.push(u);
-                    mk[u] = true;
-                }
-                else if (dist[v] + 1 == dist[u]) {
-                    p[u].insert(v);
-                }
-            }
-        }
-    };
     bfs(pol[k - 1]);
     ll cntmn = 0, cntmx = 0;
 
