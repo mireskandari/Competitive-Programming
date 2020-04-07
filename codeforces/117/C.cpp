38647ec4058cb91e00d8936be3c98a60763bec03
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-04-07 06:37:00 -0400

    Add solution for problem `C - Cycle` Link: https://codeforces.com/contest/117/problem/C Tags: dfs and similar, graphs, *2000 Ref: https://codeforces.com/contest/117/submission/75734140

---
+++ codeforces/117/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define all(v) v.begin(),v.end()
+#define X first
+#define Y second
+#define rep(x, y, z) for(ll (z) = (x); (z) < (y); (z)++)
+
+int constexpr N = 5 * 1e3;
+int constexpr MOD = 1e9 + 7;
+int constexpr INF = 1e9;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n);
+    vector<char> mark(n, false);
+    vector<vector<char>> adj(n, vector<char>(n, false));
+
+    rep (0, n, i) {
+        rep (0, n, j) {
+            char c;
+            cin >> c;
+//            cerr << c << ' ';
+            adj[i][j] = c - '0';
+            if (c - '0') g[i].push_back(j);
+        }
+    }
+    function<tuple<int, int, int>(int, int)> dfs;
+    dfs = [&](int v, int p) -> tuple<int, int, int> {
+        mark[v] = true;
+
+        for (auto u : g[v]) {
+            if (p != -1 && adj[u][p]) {
+                return {p, v, u};
+            }
+            if (!mark[u]) {
+                int v1, v2, v3;
+                tie(v1, v2, v3) = dfs(u, v);
+                if (v1 != -1) return {v1, v2, v3};
+            }
+        }
+        return {-1, -1, -1};
+    };
+    rep (0, n, i) {
+        if (!mark[i]) {
+            int v1, v2, v3;
+            tie(v1, v2, v3) = dfs(i, -1);
+
+            if (v1 != -1) return cout << v1 + 1 << ' ' << v2 + 1 << ' ' << v3 + 1, 0;
+        }
+    }
+    cout << -1;
+    return 0;
+}
