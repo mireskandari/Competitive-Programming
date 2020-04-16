b6cd9305f9a079ba914f1a48b1bc1dafe74379d0
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-04-16 07:08:00 -0400

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/76961529

---
+++ codeforces/1337/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define all(v) v.begin(),v.end()
+#define X first
+#define Y second
+#define rep(x, y, z) for(ll (z) = (x); (z) < (y); (z)++)
+
+int constexpr N = 1e5;
+int constexpr MOD = 1e9 + 7;
+int constexpr INF = 1e9;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << b << ' ' << c << ' ' << c << '\n';
+    }
+    return 0;
+}
