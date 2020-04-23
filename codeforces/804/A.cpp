8f9f584abc83c95972ce917bf0e3f53b54471bda
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-04-23 08:17:00 -0400

    Add solution for problem `A - Find Amir` Link: https://codeforces.com/contest/804/problem/A Tags: constructive algorithms, greedy, math, *1000 Ref: https://codeforces.com/contest/804/submission/77750866

---
+++ codeforces/804/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    cout << (n & 1 ? n / 2 : n / 2 - 1);
+    return 0;
+}
