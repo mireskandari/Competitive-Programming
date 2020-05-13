373a0d54d1016d63b37aab65daf6cf3015576e21
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-05-13 14:33:00 -0400

    Add solution for problem `C - Creative Snap` Link: https://codeforces.com/contest/1111/problem/C Tags: binary search, brute force, divide and conquer, math, *1700 Ref: https://codeforces.com/contest/1111/submission/80011139

---
+++ codeforces/1111/C.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define all(v) v.begin(),v.end()
+#define X first
+#define Y second
+#define Len(x) (static_cast<int>(x.size()))
+
+/* <not_serious> */
+template<typename T>
+class number_iterator : iterator<forward_iterator_tag, T> {
+public:
+    number_iterator(T _v) : v(_v), step(1) {}
+    number_iterator(T _v, T _step) : v(_v), step(_step) {}
+    operator T &() { return v; }
+    T operator*() const { return v; }
+    number_iterator operator++() {
+        v += step;
+        return *this;
+    }
+    bool operator!=(number_iterator rhs) {
+        return step > 0 ? v < rhs.v : v > rhs.v;
+    }
+private:
+    T v, step;
+};
+
+template<typename T = int>
+class range : pair<T, T> {
+public:
+    using ptt = pair<T, T>;
+    range(T begin, T end) : ptt(begin, max(begin, end)), step(1) {}
+    range(T end) : ptt(0, max<T>(0, end)), step(1) {}
+    range(T begin, T end, T _step) :
+            ptt(begin, _end(begin, end, _step)), step(_step) {}
+    number_iterator<T> begin() const {
+        return number_iterator<T>(this->first, step);
+    }
+    number_iterator<T> end() const {
+        return number_iterator<T>(this->second, step);
+    }
+private:
+    T step;
+    T _end(T begin, T end, T _step) {
+        return _step > 0 ? max(begin, end) : min(begin, end - 1);
+    }
+};
+
+template<typename T = int>
+class rrange : public range<T> {
+public:
+    rrange(T end, T begin) : range<T>(end, begin, -1) {}
+    rrange(T end) : range<T>(end, 0, -1) {}
+};
+
+template<class T = int, class... Args>
+auto vec(size_t n, Args &&... args) {
+    if constexpr(sizeof...(args) == 1)
+        return vector<T>(n, T(args...));
+    else
+        return vector(n, vec<T>(args...));
+}
+/* </not_serious> */
+int constexpr MOD = 1e9 + 7;
+ll constexpr INF = 1e9;
+
+vector<ll> in;
+ll n, k, a, b;
+
+ll Solve(ll l, ll r) {
+    ll low = lower_bound(all(in), l) - in.begin();
+    ll upper = upper_bound(all(in), r) - in.begin();
+    ll sz = upper - low;
+    ll fans = sz > 0 ? b * sz * (r - l + 1) : a;
+    if (r - l <= 0 || sz == 0) return fans;
+    ll m = (r + l) >> 1;
+    return min(Solve(l, m) + Solve(m + 1, r), fans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> k >> a >> b;
+    in.resize(k);
+    for (auto i : range(k)) cin >> in[i];
+    sort(all(in));
+    cout << Solve(1, (1LL << n));
+    return 0;
+}
