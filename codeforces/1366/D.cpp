0dccbbc803f6ec4fbd43a6ce200bc8bcd291c803
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-06-13 05:55:00 -0400

    Add solution for problem `D - Two Divisors` Link: https://codeforces.com/contest/1366/problem/D Tags: constructive algorithms, math, number theory, *2000 Ref: https://codeforces.com/contest/1366/submission/83598514

---
+++ codeforces/1366/D.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HELLO_PEOPLE
+#define cerr if(0) cout
+#endif
+
+using ll = long long;
+using ld = long double;
+
+#define reps(a, b, x, s) \
+        for (decay<decltype(b)>::type x = a; x < b; x += s)
+#define pers(b, a, x, s) \
+        for (decay<decltype(b)>::type x = b; x >= a; x -= s)
+
+#define rep(a, b, x) reps(a, b, x, 1)
+#define per(b, a, x) pers(b, a, x, 1)
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define X first
+#define Y second
+
+template <class C>
+inline int constexpr __size(C c) { return c.size(); }
+#define size __size
+
+template <int D, class T>
+struct vec : public vector<vec<D - 1, T>> {
+    template <class... Args>
+    vec(int n = 0, Args... a) :
+            vector<vec<D - 1, T>>(n, vec<D - 1, T>(a...)) {}
+};
+
+template <typename T>
+struct vec<1, T> : public vector<T> {
+    vec(int n = 0, T const &val = T()) :
+            vector<T>(n, val) {}
+};
+// now start
+
+template<class Ch, class Tr, class Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr> &os,
+                Container const &x) {
+    os << "{ ";
+    for (auto &y : x) os << y << " ";
+    return os << "}";
+}
+
+template<class X, class Y>
+ostream& operator<<(ostream &os, pair<X, Y> const &p) {
+    return os << "[ " << p.first << ", " << p.second << " ]";
+}
+
+template<class Ch, class Tr, class Tuple, int N>
+struct Tp {
+    static void print(basic_ostream<Ch, Tr> &os, Tuple const &t) {
+        Tp<Ch, Tr, Tuple, N - 1>::print(os, t);
+        os << std::get<N - 1>(t) << " ";
+    }
+};
+
+template<class Ch, class Tr, class Tuple>
+struct Tp<Ch, Tr, Tuple, 0> {
+    static void print(basic_ostream<Ch, Tr> &os, Tuple const &t) {}
+};
+
+template<class Ch, class Tr, class... Args>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr> &os,
+                tuple<Args...> const &t) {
+    os << "[ ";
+    Tp<Ch, Tr, tuple<Args...>, sizeof...(Args)>::print(os, t);
+    return os << "]";
+}
+
+ll const INF = 1e14;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (auto &i : a) cin >> i;
+    int N = *max_element(all(a));
+    vector<int> spf(N + 1, 0);
+    spf[1] = 1;
+    rep (2, N + 1, i) {
+        if (spf[i] == 0) {
+            spf[i] = i;
+            reps (2 * i, N + 1, j, i) {
+                if (spf[j] == 0) spf[j] = i;
+            }
+        }
+    }
+    vector<pair<int, int>> ready(N + 1, {-1, -1});
+    vector<int> d1(n), d2(n);
+    rep (0, n, i) {
+        if (ready[a[i]].X != -1) {
+            tie(d1[i], d2[i]) = ready[a[i]];
+            continue;
+        }
+        int temp = a[i];
+        int fspf = spf[a[i]];
+        if (fspf == a[i]) {
+            ready[a[i]] = {-1, -1};
+            tie(d1[i], d2[i]) = ready[a[i]];
+            continue;
+        }
+        while (a[i] % fspf == 0) a[i] /= fspf;
+        if (fspf != 1 && a[i] != 1 && __gcd<ll>(a[i] + fspf, temp) == 1) {
+            ready[temp] = {a[i], fspf};
+            tie(d1[i], d2[i]) = ready[temp];
+        }
+    }
+    rep (0, n, i) cout << (d1[i] == 0 ? -1 : d1[i]) << ' ';
+    cout << '\n';
+    rep (0, n, i) cout << (d2[i] == 0 ? -1 : d2[i]) << ' ';
+    return 0;
+}
