c2e7f0172a706c7e5a5931f949df972de796c6a8
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-06-22 08:00:00 -0400

    Add solution for problem `D - Inversion Counting` Link: https://codeforces.com/contest/911/problem/D Tags: brute force, math, *1800 Ref: https://codeforces.com/contest/911/submission/84659326

---
+++ codeforces/911/D.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HELLO_PEOPLE
+#define cerr if(0) cout
+#endif
+
+using ll = long long;
+using ld = long double;
+
+#define all(v) (v).begin(), (v).end()
+#define X first
+#define Y second
+
+template<class T> struct Nit { T _v, _s; Nit(T v, T s) : _v(v), _s(s) {} operator T &() { return _v; } T operator *() const { return _v; } Nit &operator++() { _v += _s; return *this; } bool operator!=(Nit &a) { return (_s > 0 ? _v < a._v : _v >= a._v);} };
+template<class T = int> struct range { T _b, _e, _s; range(T e) : _b(0), _e(e), _s(1) {} range(T b, T e, T s = 1) : _b(b), _e(e), _s(s) {} Nit<T> begin() { return Nit<T>(_b, _s); } Nit<T> end() { return Nit<T>(_e, _s); } };
+template<class T = int> struct rrange : range<T> { rrange(T e, T b, T s = 1) : range<T>(e, b, -s) {} rrange(T e) : range<T>(e, 0, -1) {} };
+
+template<int D, class T> struct vec : public vector<vec<D - 1, T>> { template<class... Args> vec(int n = 0, Args... a) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(a...)) {} };
+template<class T> struct vec<1, T> : public vector<T> { vec(int n = 0, T const &val = T()) : vector<T>(n, val) {} };
+
+
+// now start
+ll const INF = 1e14;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto i : range(n)) cin >> a[i];
+    bool par = false;
+    for (auto i : range(n)) {
+        for (auto j : range(i)) {
+            if (a[i] < a[j]) par = !par;
+        }
+    }
+    int m;
+    cin >> m;
+    while (m--) {
+        int l, r;
+        cin >> l >> r;
+        if (((r - l + 1) / 2) & 1) par = !par;
+        cout << (par ? "odd\n" : "even\n");
+    }
+    return 0;
+}
