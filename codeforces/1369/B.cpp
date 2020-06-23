22d7c32b3318b967823b10ae0db7e9de56a67965
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-06-23 13:47:00 -0400

    Add solution for problem `B - AccurateLee` Link: https://codeforces.com/contest/1369/problem/B Tags: greedy, implementation, strings, *1200 Ref: https://codeforces.com/contest/1369/submission/84825846

---
+++ codeforces/1369/B.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HELLO_PEOPLE
+#define cerr if(0) cout
+#endif
+
+using ll = long long;
+using ld = long double;
+
+#define all(v) (v).begin(), (v).end()
+#define X first
+#define Y second
+
+template<class T> struct Nit { T _v, _s; Nit(T v, T s) : _v(v), _s(s) {} operator T &() { return _v; } T operator *() const { return _v; } Nit &operator++() { _v += _s; return *this; } bool operator!=(Nit &a) { return (_s > 0 ? _v < a._v : _v >= a._v);} };
+template<class T = int> struct range { T _b, _e, _s; range(T e) : _b(0), _e(e), _s(1) {} range(T b, T e, T s = 1) : _b(b), _e(e), _s(s) {} Nit<T> begin() { return Nit<T>(_b, _s); } Nit<T> end() { return Nit<T>(_e, _s); } };
+template<class T = int> struct rrange : range<T> { rrange(T e, T b, T s = 1) : range<T>(e, b, -s) {} rrange(T e) : range<T>(e, 0, -1) {} };
+
+template<int D, class T> struct vec : public vector<vec<D - 1, T>> { template<class... Args> vec(int n = 0, Args... a) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(a...)) {} };
+template<class T> struct vec<1, T> : public vector<T> { vec(int n = 0, T const &val = T()) : vector<T>(n, val) {} };
+
+
+template<int N, class T> void print_tuple(ostream &, T const &) { }
+template<int N, class T, class H, class... Args> void print_tuple(ostream &os, T const &t) { if (N) os << " ,"; os << std::get<N>(t); print_tuple<N + 1, T, Args...>(os, t); }
+template<class... Args> ostream &operator<<(ostream &os,const tuple<Args...> &t){ os << "{"; print_tuple<0, tuple<Args...>, Args...>(os, t); return os << "}"; }
+template<class Ch, class Tr, class C> basic_ostream<Ch, Tr> &operator<<(basic_ostream<Ch, Tr> &os, C const &x) { os << "{ "; for (auto &y : x) os << y << " "; return os << "}"; }
+template<class I, class J> ostream &operator<<(ostream &os, pair<I, J> const &p) { return os << "[ " << p.first << ", " << p.second << " ]"; }
+
+// now start
+ll const INF = 1e14;
+
+void run_case() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int cnt0 = 0, i = 0;
+    while (i < n && s[i] == '0') {
+        cnt0++;
+        i++;
+    }
+
+    int cnt1 = 0;
+    i = n - 1;
+    while (i >= 0 && s[i] == '1') {
+        cnt1++;
+        i--;
+    }
+
+    if (cnt0 + cnt1 == n) {
+        cout << s << '\n';
+        return;
+    }
+    cout << string(cnt0 + 1, '0') << string(cnt1, '1') << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--) run_case();
+    return 0;
+}
