5dbaeeace2225bfce2f710ff294c049661b56d34
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-06-27 10:29:00 -0400

    Add solution for problem `A - Donut Shops` Link: https://codeforces.com/contest/1373/problem/A Tags: greedy, implementation, math, *1000 Ref: https://codeforces.com/contest/1373/submission/85224775

---
+++ codeforces/1373/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HELLO_PEOPLE
+#define cerr if(0) cout
+#endif
+
+using ll = long long;
+using ld = long double;
+
+#define all(v) (v).begin(), (v).end()
+#define X first
+#define Y second
+
+template<class T> struct Nit { T _v, _s; Nit(T v, T s) : _v(v), _s(s) {} operator T &() { return _v; } T operator *() const { return _v; } Nit &operator++() { _v += _s; return *this; } bool operator!=(Nit &a) { return (_s > 0 ? _v < a._v : _v >= a._v);} };
+template<class T = int> struct range { T _b, _e, _s; range(T e) : _b(0), _e(e), _s(1) {} range(T b, T e, T s = 1) : _b(b), _e(e), _s(s) {} Nit<T> begin() { return Nit<T>(_b, _s); } Nit<T> end() { return Nit<T>(_e, _s); } };
+template<class T = int> struct rrange : range<T> { rrange(T e, T b, T s = 1) : range<T>(e, b, -s) {} rrange(T e) : range<T>(e, 0, -1) {} };
+
+template<int D, class T> struct vec : public vector<vec<D - 1, T>> { template<class... Args> vec(int n = 0, Args... a) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(a...)) {} };
+template<class T> struct vec<1, T> : public vector<T> { vec(int n = 0, T const &val = T()) : vector<T>(n, val) {} };
+
+
+// now start
+ll const INF = 1e14;
+
+void run_case() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    ll ans1, ans2;
+    if (c / b >= a) {
+        ans2 = -1;
+    } else {
+        ans2 = b;
+    }
+    if (c <= a) {
+        ans1 = -1;
+    } else {
+        ans1 = 1;
+    }
+    cout << ans1 << ' ' << ans2 << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--) run_case();
+    return 0;
+}
