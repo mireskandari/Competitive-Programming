fa45af15863070014cda647d5b128c6861ba8f50
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-06-29 08:32:00 -0400

    Add solution for problem `C - Move Brackets` Link: https://codeforces.com/contest/1374/problem/C Tags: greedy, strings, *1000 Ref: https://codeforces.com/contest/1374/submission/85456361

---
+++ codeforces/1374/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HELLO_PEOPLE
+#define cerr if(0) cout
+#endif
+
+using ll = long long;
+using ld = long double;
+
+#define all(v) (v).begin(), (v).end()
+#define X first
+#define Y second
+
+// magic
+template<class T> struct Nit { T _v, _s; Nit(T v, T s) : _v(v), _s(s) {} operator T &() { return _v; } T operator *() const { return _v; } Nit &operator++() { _v += _s; return *this; } bool operator!=(Nit &a) { return (_s > 0 ? _v < a._v : _v >= a._v); } }; template<class T = int> struct range { T _b, _e, _s; range(T e) : _b(0), _e(e), _s(1) {} range(T b, T e, T s = 1) : _b(b), _e(e), _s(s) {} Nit<T> begin() { return Nit<T>(_b, _s); } Nit<T> end() { return Nit<T>(_e, _s); } }; template<class T = int> struct rrange : range<T> { rrange(T e, T b, T s = 1) : range<T>(e, b, -s) {} rrange(T e) : range<T>(e, 0, -1) {} }; template<class T> struct y_res { T t; y_res(T &&t) : t(forward<T>(t)) {} template<class... Args> decltype(auto) operator() (Args &&...args) const { return t(*this, forward<Args>(args)...); } }; template<class T> decltype(auto) y_comb(T &&t) { return y_res<decay_t<T>>(forward<T>(t)); } template<int D, class T> struct vec : public vector<vec<D - 1, T>> { template<class... Args> vec(int n = 0, Args... a) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(a...)) {} }; template<class T> struct vec<1, T> : public vector<T> { vec(int n = 0, T const &val = T()) : vector<T>(n, val) {} };
+
+// now start
+ll constexpr INF = 1e14;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    auto &&run_case = []() {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        vector<int> a(n);
+        for (auto i : range<>(n)) {
+            a[i] = s[i] == '(' ? 0 : 1;
+        }
+
+        stack<int> st;
+        for (auto i : range<>(n)) {
+            if (!st.empty() && a[i] == 1 && st.top() == 0) {
+                st.pop();
+            } else {
+                st.push(a[i]);
+            }
+        }
+
+        cout << st.size() / 2 << '\n';
+    };
+
+    int tc;
+    cin >> tc;
+    while (tc--) run_case();
+    return 0;
+}
