51e2bccbca764707bb083a5f6e36774f1be47f91
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-06-29 10:13:00 -0400

    Add solution for problem `C - Basketball Exercise` Link: https://codeforces.com/contest/1195/problem/C Tags: dp, *1400 Ref: https://codeforces.com/contest/1195/submission/85464878

---
+++ codeforces/1195/C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef HELLO_PEOPLE
+#define cerr if(0) cout
+#endif
+
+using ll = long long;
+using ld = long double;
+
+#define all(v) (v).begin(), (v).end()
+#define X first
+#define Y second
+
+// magic
+template<class T> struct Nit { T _v, _s; Nit(T v, T s) : _v(v), _s(s) {} operator T &() { return _v; } T operator *() const { return _v; } Nit &operator++() { _v += _s; return *this; } bool operator!=(Nit &a) { return (_s > 0 ? _v < a._v : _v >= a._v); } }; template<class T = int> struct range { T _b, _e, _s; range(T e) : _b(0), _e(e), _s(1) {} range(T b, T e, T s = 1) : _b(b), _e(e), _s(s) {} Nit<T> begin() { return Nit<T>(_b, _s); } Nit<T> end() { return Nit<T>(_e, _s); } }; template<class T = int> struct rrange : range<T> { rrange(T e, T b, T s = 1) : range<T>(e, b, -s) {} rrange(T e) : range<T>(e, 0, -1) {} }; template<class T> struct y_res { T t; y_res(T &&t) : t(forward<T>(t)) {} template<class... Args> decltype(auto) operator() (Args &&...args) const { return t(*this, forward<Args>(args)...); } }; template<class T> decltype(auto) y_comb(T &&t) { return y_res<decay_t<T>>(forward<T>(t)); } template<int D, class T> struct vec : public vector<vec<D - 1, T>> { template<class... Args> vec(int n = 0, Args... a) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(a...)) {} }; template<class T> struct vec<1, T> : public vector<T> { vec(int n = 0, T const &val = T()) : vector<T>(n, val) {} };
+
+// now start
+ll constexpr INF = 1e14;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#define int ll
+
+    int n;
+    cin >> n;
+    vec<2, int> a(n, 2);
+    for (auto &v : a) cin >> v[0];
+    for (auto &v : a) cin >> v[1];
+
+    vec<2, int> dp(n + 1, 2);
+    dp[0][0] = 0, dp[0][1] = 0;
+    for (auto i : range<>(1, n + 1)) {
+        for (auto x : range<>(2)) dp[i][x] = max(dp[i - 1][!x] + a[i - 1][x], dp[i - 1][x]);
+    }
+
+    cout << max(dp[n][0], dp[n][1]);
+    return 0;
+}
