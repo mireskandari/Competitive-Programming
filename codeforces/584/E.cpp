3e1ed92c39a0656a861e664968815b1e84c5308e
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-07-26 12:30:00 -0400

    Add solution for problem `E - Anton and Ira` Link: https://codeforces.com/contest/584/problem/E Tags: constructive algorithms, greedy, math, *2300 Ref: https://codeforces.com/contest/584/submission/88072645

--- codeforces/584/E.cpp
+++ codeforces/584/E.cpp
@@ -1,60 +1,71 @@
 #include <bits/stdc++.h>
 
-#define SZ(x) ((int)x.size())
-#define PB push_back
-#define MP make_pair
+using namespace std;
+
+#ifndef LOCAL
+#define cerr if(0) cerr
+#endif
+
+#define all(v) (v).begin(), (v).end()
 #define X first
 #define Y second
 
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
+using ll = long long;
+using ld = long double;
 
-const int N = 2000 + 100;
+template <class T = int> using V = vector<T>;
+template <class T = int> using VV = V<V<T>>;
+template <class T = int> using PP = pair<T, T>;
 
-int n, p[N], q[N];
 
-vector <pii> sw;
-
-int a[N];
-
-void f(){
-	for(int i = 0; i < n; ++i){
-		for(int j = i + 1; j <= a[i]; ++j){
-			if(a[j] <= i){
-				swap(p[i], p[j]);
-				sw.PB(MP(i, j));
-				swap(a[i], a[j]);
-			}
-		}
-	}
-}
-
-bool g(){
-	for(int i = 0; i < n; ++i)
-		if(p[i] != q[i]) return 1;
-	return 0;
-}
-
-int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	for(int i = 0; i < n; ++i) cin >> p[i];
-	for(int i = 0; i < n; ++i) cin >> q[i];
-	ll ans = 0;
-	for(int i = 0; i < n; ++i)
-		for(int j = 0; j < n; ++j)
-			if(p[i] == q[j])
-				ans += abs(i - j), a[i] = j;
-	cout << ans / 2 << "\n";
-	for (int i = 0; i < 2000; i++) {
-    f();
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  V<> a(n), b(n);
+  V<> dst(n + 1), idx(n + 1);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    idx[a[i]] = i;
   }
-	cout << SZ(sw) << "\n";
-	for(int i = 0; i < SZ(sw); ++i){
-		cout << sw[i].X + 1 << " " << sw[i].Y + 1 << "\n";
-	}
-	return 0;
+  for (int i = 0; i < n; i++) {
+    cin >> b[i];
+    dst[b[i]] = i;
+  }
+  // maGiC
+  V<PP<>> ans;
+  int cost = 0;
+  for (int rep = 0; rep < 2; rep++) { 
+    for (int i = 1; i <= n; i++) {
+      if (dst[i] > idx[i]) {
+        for (int j = idx[i] + 1; j <= dst[i]; j++) {
+          if (j < idx[i] || dst[a[j]] > idx[i]) {
+            continue;
+          }
+          ans.emplace_back(idx[i], j);
+          int temp = a[j];
+          swap(a[idx[i]], a[j]);
+          swap(idx[i], idx[temp]);
+          cost += abs(ans.back().X - ans.back().Y);
+        }
+      } else if (dst[i] < idx[i]) {
+        for (int j = idx[i] - 1; j >= dst[i]; j--) {
+          if (j > idx[i] || dst[a[j]] < idx[i]) {
+            continue;
+          }
+          ans.emplace_back(idx[i], j);
+          int temp = a[j];
+          swap(a[idx[i]], a[j]);
+          swap(idx[i], idx[temp]);
+          cost += abs(ans.back().X - ans.back().Y);
+        }
+      }
+    }
+  }
+  cout << cost << '\n' << ans.size() << '\n';
+  for (auto &p : ans) {
+    cout << p.X + 1 << ' ' << p.Y + 1 << '\n';
+  }
+  return 0;
 }
-
