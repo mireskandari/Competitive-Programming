1f5682700dc09c58fdc9fd5b2ed71cff50f7071f
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-08-12 06:03:00 -0400

    Add solution for problem `C - Mr. Kitayuta, the Treasure Hunter` Link: https://codeforces.com/contest/505/problem/C Tags: dfs and similar, dp, two pointers, *1900 Ref: https://codeforces.com/contest/505/submission/89640647

--- codeforces/505/C.cpp
+++ codeforces/505/C.cpp
@@ -9,35 +9,39 @@ typedef long long ll;
 
 int constexpr N = 30010;
 
-int n, d;
-int dp[N][800];
-int has[N];
-int a[N];
-
-int solve(int i, int j) {
-  int delta = j - d + 380;
-  if (i >= N) return 0;
-  if (dp[i][delta] != -1) return dp[i][delta];
-
-  if (j != 1) {
-    dp[i][delta] = max({solve(i + j + 1, j + 1), solve(i + j, j), solve(i + j - 1, j - 1)}) + has[i];
-  } else {
-    dp[i][delta] = max(solve(i + j + 1, j + 1), solve(i + j, j)) + has[i];
-  }
-  return dp[i][delta];
-}
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
+
+  int n, d;
   cin >> n >> d;
+  static int a[N];
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
   }
+
+  static int has[N];
   for (int i = 0; i < n; ++i) {
     has[a[i]]++;
   }
-  memset(dp, -1, sizeof(dp));  
+  
+  static int dp[N][800];
+  memset(dp, -1, sizeof(dp));
+
+  // was hard to fuck with when iterative
+  function<int(int, int)> solve = [&](int i, int j) {
+    int delta = j - d + 380;
+    if (i >= N) return 0;
+    if (dp[i][delta] != -1) return dp[i][delta];
+
+    if (j != 1) {
+      dp[i][delta] = max({solve(i + j + 1, j + 1), solve(i + j, j), solve(i + j - 1, j - 1)}) + has[i];
+    } else {
+      dp[i][delta] = max(solve(i + j + 1, j + 1), solve(i + j, j)) + has[i];
+    }
+    return dp[i][delta];
+  };
+  
   cout << solve(d, d);
   return 0;
 }
