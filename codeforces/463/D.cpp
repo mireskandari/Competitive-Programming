c2055a60a0b6e155fcc195a528b31f13c46cfe89
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-08-29 08:15:00 -0400

    Add solution for problem `D - Gargari and Permutations` Link: https://codeforces.com/contest/463/problem/D Tags: dfs and similar, dp, graphs, implementation, *1900 Ref: https://codeforces.com/contest/463/submission/91283201

--- codeforces/463/D.cpp
+++ codeforces/463/D.cpp
@@ -1,58 +1,37 @@
-#include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-#define ERR(a...) fprintf(stderr, a)
+#ifndef LOCAL
+#define cerr if(0) cerr
+#endif
 
-constexpr int N = 1010;
-
-vector<int> g[N];
-int a[N][10], dp[N], pos[N][50];
-char mark[N];
-
-void Go(int v) {
-  mark[v] = 1;
-  for (auto &u : g[v]) {
-    if (mark[u] == 0) {
-      Go(u);
-    }
-    dp[v] = max(dp[v], dp[u] + 1);
-  }
-  mark[v] = 2;
-}
+#define all(v) (v).begin(), (v).end()
+#define vc vector
 
 int main() {
-  int n, k;
-  scanf("%d %d", &n, &k);
-  for (int j = 1; j <= k; ++j) {
-    for (int i = 1; i <= n; ++i) {
-      scanf("%d", &a[i][j]);
-    }
-  }
-  for (int j = 1; j <= k; ++j) {
-    for (int i = 1; i <= n; ++i) {
-      pos[a[i][j]][j] = i;
-    }
-  }
-  for (int i = 1; i <= n; ++i) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k; cin >> n >> k;
+  vc<vc<int>> a(k, vc<int>(n));
+  for (auto &v : a) for (auto &i : v) cin >> i;
+  vc<vc<int>> pos(k, vc<int>(n + 1));
+  for (int i = 0; i < k; ++i) {
     for (int j = 1; j <= n; ++j) {
-      if (i == j) continue;
-      bool ok = true;
-      for (int x = 1; x <= k; ++x) {
-        if (pos[i][x] > pos[j][x]) {
-          ok = false;
-          break;
-        }
-      }
-      if (!ok) continue;
-      g[j].push_back(i);
+      pos[i][a[i][j - 1]] = j;
     }
   }
-  fill_n(dp, N, 1);
+  vc<int> dp(n + 1);
   for (int i = 1; i <= n; ++i) {
-    if (mark[i] == 0) Go(i);
+    dp[i] = 1;
+    for (int j = 1; j < i; ++j) {
+      bool correct = true;
+      for (int x = 0; x < k; ++x) {
+        correct &= pos[x][a[0][j - 1]] < pos[x][a[0][i - 1]];
+      }
+      if (!correct) continue;
+      dp[i] = max(dp[i], dp[j] + 1);
+    }
   }
-  printf("%d", *max_element(dp, dp + N));
+  cout << *max_element(all(dp));
   return 0;
 }
