90ba6ba045cb32d15124319dd122bd1732f8296a
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-09-04 02:13:00 -0400

    Add solution for problem `B - Edge Weight Assignment` Link: https://codeforces.com/contest/1338/problem/B Tags: bitmasks, constructive algorithms, dfs and similar, greedy, math, trees, *1800 Ref: https://codeforces.com/contest/1338/submission/91768544

--- codeforces/1338/B.cpp
+++ codeforces/1338/B.cpp
@@ -14,62 +14,51 @@ void err(T a, A ...b) {
 
 #define all(v) (v).begin(), (v).end()
 #define long int64_t
+using vll = vector<long>;
+using vi = vector<int>;
 
 
-long n, m;
-vector<vector<long>> g;
-vector<long> depth;
-vector<bool> leaf, mark, has_leaf;
-
-void Reset() {
-    g.assign(n, vector<long>());
-    depth.resize(n);
-    leaf.resize(n, false);
-    has_leaf.assign(n, false);
-    mark.assign(n, false);
-}
-
-void Dfs(long v) {
-    mark[v] = true;
-    for (auto &u : g[v]) {
-        if (!mark[u]) {
-            depth[u] = depth[v] + 1;
-            if (leaf[u]) has_leaf[v] = true;
-            Dfs(u);
-        }
-    }
-}
-
-signed main() {
+int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-
-    cin >> n;
-    m = n - 1;
-    Reset();
-    long st = 0;
-    for (int i = 0; i < m; ++i) {
+    long n; cin >> n;
+    vector<vll> g(n);
+    for (int i = 0; i < n - 1; ++i) {
         int v, u; cin >> v >> u;
         g[--v].emplace_back(--u);
         g[u].emplace_back(v);
     }
-
+    vector<bool> leaf(n), has_leaf(n);
+    long st = 0;
     for (int i = 0; i < n; ++i) {
-        leaf[i] = (int) g[i].size() <= 1;
-        if (!leaf[i]) st = i;
-    }
-
-    depth[st] = 0;
-    Dfs(st);
-
-    vector<long> parity(2);
-    for (int i = 0; i < n; ++i) {
-        if (leaf[i]) {
-            ++parity[depth[i] % 2];
+        if ((int) g[i].size() <= 1) {
+            leaf[i] = true;
+            has_leaf[g[i][0]] = true;
+        } else {
+            st = i;
         }
     }
-    
-    cout << (parity[0] && parity[1] ? 3 : 1) << ' ';
-    cout << m - count(all(leaf), true) + count(all(has_leaf), true);
-
+    vector<bool> mark(n);
+    vll depth(n);
+    function<void(int)> Dfs = [&](int v) {
+        mark[v] = true;
+        for (auto &u : g[v]) {
+            if (!mark[u]) {
+                depth[u] = depth[v] + 1;
+                Dfs(u);
+            }
+        }
+    };
+    Dfs(st);
+    long cntl = (int) count(all(leaf), true);
+    long cntnl = (int) count(all(has_leaf), true);
+    vll parity(2);
+    for (int i = 0; i < n; ++i) {
+        if (leaf[i]) {
+            parity[depth[i] % 2]++;
+        }
+    }
+    cout << (parity[0] > 0 && parity[1] > 0 ? 3 : 1);
+    cout << ' ' << n - 1 - cntl + cntnl;
+    // what the fuck
     return 0;
 }
