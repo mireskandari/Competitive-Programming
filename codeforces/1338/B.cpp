3ea175071eac7ad23b0ab3f876d49a1d7b175e18
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-09-04 02:26:00 -0400

    Add solution for problem `B - Edge Weight Assignment` Link: https://codeforces.com/contest/1338/problem/B Tags: bitmasks, constructive algorithms, dfs and similar, greedy, math, trees, *1800 Ref: https://codeforces.com/contest/1338/submission/91769379

--- codeforces/1338/B.cpp
+++ codeforces/1338/B.cpp
@@ -16,17 +16,17 @@ void err(T a, A ...b) {
 #define long int64_t
 
 
-long n;
+long n, m;
 vector<vector<long>> g;
 vector<long> depth;
-vector<bool> mark, has_leaf, leaf;
+vector<bool> leaf, mark, has_leaf;
 
 void Reset() {
     g.assign(n, vector<long>());
     depth.resize(n);
-    mark.assign(n, false);
-    leaf.assign(n, false);
+    leaf.resize(n, false);
     has_leaf.assign(n, false);
+    mark.assign(n, false);
 }
 
 void Dfs(long v) {
@@ -34,52 +34,42 @@ void Dfs(long v) {
     for (auto &u : g[v]) {
         if (!mark[u]) {
             depth[u] = depth[v] + 1;
+            if (leaf[u]) has_leaf[v] = true;
             Dfs(u);
         }
     }
-}
-
-bool CheckMin() {
-    for (int i = 0; i < n; ++i) {
-        if ((int) g[i].size() <= 1) {
-            leaf[i] = true;
-            has_leaf[g[i][0]] = true;
-        }
-    }
-
-    vector<long> parity(2);
-    for (int i = 0; i < n; ++i) {
-        if (leaf[i]) parity[depth[i] % 2]++;
-    }
-
-    return parity[0] && parity[1];
-}
-
-int GetMax() {
-    return n - 1 - count(all(leaf), true) + count(all(has_leaf), true);
 }
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
 
     cin >> n;
+    m = n - 1;
     Reset();
-
-    for (int i = 0; i < n - 1; ++i) {
+    long st = 0;
+    for (int i = 0; i < m; ++i) {
         int v, u; cin >> v >> u;
         g[--v].emplace_back(--u);
         g[u].emplace_back(v);
     }
-    
-    long st = 0;
+
     for (int i = 0; i < n; ++i) {
-        if ((int) g[i].size() > 1) st = i;
+        leaf[i] = (int) g[i].size() <= 1;
+        if (!leaf[i]) st = i;
     }
+
     depth[st] = 0;
     Dfs(st);
+
+    vector<long> parity(2);
+    for (int i = 0; i < n; ++i) {
+        if (leaf[i]) {
+            ++parity[depth[i] % 2];
+        }
+    }
     
-    cout << (CheckMin() ? 3 : 1) << ' ';
-    cout << GetMax();
-    
+    cout << (parity[0] && parity[1] ? 3 : 1) << ' ';
+    cout << m - count(all(leaf), true) + count(all(has_leaf), true);
+
     return 0;
 }
