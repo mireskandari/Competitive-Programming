c72cde8cf998bdd93029c21cabebfa030214db4f
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-11-01 09:37:00 -0500

    Add solution for problem `D - Divide and Sum` Link: https://codeforces.com/contest/1445/problem/D Tags: combinatorics, math, sortings, *1900 Ref: https://codeforces.com/contest/1445/submission/97363295

---
+++ codeforces/1445/D.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+using int64 = long long;
+#define ERR(...) fprintf(stderr, __VA_ARGS__)
+
+const int MO = 998244353;
+
+int Power(int a, int b) {
+	int res = 1;
+	for (; b > 0; b >>= 1, a = 1ll * a * a % MO) {
+		if (b & 1) res = 1ll * res * a % MO;
+	}
+	return res;
+}
+
+const int N = 3e5 + 10;
+
+int a[N];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	n *= 2;
+	for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
+	sort(a, a + n);
+	int ans = 0;
+	for (int i = 0; i < n / 2; ++i) {
+		ans += (a[i + n / 2] - a[i]) % MO;
+		if (ans >= MO) ans -= MO;
+	}
+	int res = 1;
+	for (int i = 2; i <= n; ++i) res = (1ll * res * i) % MO;
+	for (int i = 2; i <= n / 2; ++i) res = (1ll * res * Power(i, MO - 2)) % MO;
+	for (int i = 2; i <= n / 2; ++i) res = (1ll * res * Power(i, MO - 2)) % MO;
+	printf("%lld", 1ll * ans * res % MO);
+	return 0;
+}
