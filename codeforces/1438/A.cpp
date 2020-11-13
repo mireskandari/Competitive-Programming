4e2573e7f0e2a461a060c739074e029b8745d290
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-11-13 09:36:00 -0500

    Add solution for problem `A - Specific Tastes of Andre ` Link: https://codeforces.com/contest/1438/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1438/submission/98281869

---
+++ codeforces/1438/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+int len(const T& a) {
+	return a.size();
+}
+using ll = long long;
+
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; ++i) {
+			cout << 1 << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
+
