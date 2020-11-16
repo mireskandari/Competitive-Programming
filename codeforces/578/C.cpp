18bd8215828b571ca9eb9a2fee228f77f2f559f6
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2020-11-16 03:19:00 -0500

    Add solution for problem `C - Weakness and Poorness` Link: https://codeforces.com/contest/578/problem/C Tags: ternary search, *2000 Ref: https://codeforces.com/contest/578/submission/98530958

--- codeforces/578/C.cpp
+++ codeforces/578/C.cpp
@@ -1,4 +1,3 @@
-#pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,17 +7,13 @@ int len(const T& a) {
 }
 using ll = long long;
 
-constexpr int N = 2e5 + 10;
 constexpr long double EPS = 1e-7;
 
-int n;
-int a[N];
-
-long double Check(long double x) {
-	static long double new_a[N];
+long double Check(const vector<long double>& a, long double x) {
+	vector<long double> new_a(a.size());
 	long double bestmin = 1e8, curmin = 0;
 	long double bestmax = -1e8, curmax = 0;
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < len(a); ++i) {
 		new_a[i] = (long double) a[i] - x;
 		curmax = max((long double) 0, curmax + new_a[i]);
 		bestmax = max(bestmax, curmax);
@@ -31,20 +26,23 @@ long double Check(long double x) {
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	scanf("%d", &n);
-	for (int i = 0; i < n; ++i)
-		scanf("%d", &a[i]);
+	cout << fixed << setprecision(6);
+	int n;
+	cin >> n;
+	vector<long double> a(n);
+	for (auto& i : a)
+		cin >> i;
 	long double l = -1e4 - 10, r = 1e4 + 10;
 	for (int step = 0; step < 200; ++step) {
 		long double m1 = l + (r - l) / 3.0;
 		long double m2 = r - (r - l) / 3.0;
-		if (Check(m1) - Check(m2) > EPS) {
+		if (Check(a, m1) - Check(a, m2) > EPS) {
 			l = m1;
 		} else {
 			r = m2;
 		}
 	}
-	printf("%.6lf", (double) Check(l));
+	cout << Check(a, l);
 	return 0;
 }
 
