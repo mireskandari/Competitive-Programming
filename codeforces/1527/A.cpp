de611ce3b0e2e6064db0e81d56cb3cd6789e2672
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2021-05-20 10:36:00 -0400

    Add solution for problem `A - And Then There Were K` Link: https://codeforces.com/contest/1527/problem/A Tags: bitmasks, *800 Ref: https://codeforces.com/contest/1527/submission/116752548

---
+++ codeforces/1527/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define cerr cerr << "DEBUG "
+template <typename T> inline int len(const T &a) { return a.size(); }
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n;
+		cin >> n;
+		cout << (1 << __lg(n)) - 1 << '\n';
+	}
+}
+
