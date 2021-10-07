c726ecce1990841a60e10d91c98b51a45a903a59
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2021-10-07 15:11:00 -0400

    Add solution for problem `E - Xenia and Tree` Link: https://codeforces.com/contest/342/problem/E Tags: data structures, divide and conquer, trees, *2400 Ref: https://codeforces.com/contest/342/submission/131098446

--- codeforces/342/E.cpp
+++ codeforces/342/E.cpp
@@ -2,27 +2,30 @@
 using namespace std;
 #define cerr cerr << "DEBUG "
 
-constexpr int N = 1e5 + 10;
+constexpr int N = 1e5 + 10, SQ = 320, LG = 21;
 
-int n, que;
+int n, m, bst[N];
 vector<int> g[N];
-int dep[N], par[20][N];
+bool mark[N];
+vector<int> qs;
+int par[LG][N], dep[N];
 
 void prep(int v, int p) {
+	par[0][v] = p;
+	for (int i = 1; i < LG; ++i) {
+		par[i][v] = par[i - 1][par[i - 1][v]];
+	}
+
 	for (auto &u : g[v]) {
 		if (u != p) {
-			par[0][u] = v;
-			for (int i = 1; i < 20; ++i) {
-				par[i][u] = par[i - 1][par[i - 1][u]];
-			}
 			dep[u] = dep[v] + 1;
 			prep(u, v);
 		}
 	}
 }
 
-int getpar(int v, int k) {
-	for (int i = 0; i < 20; ++i) {
+int get_par(int v, int k) {
+	for (int i = 0; i < LG; ++i) {
 		if ((k >> i) & 1) {
 			v = par[i][v];
 		}
@@ -34,129 +37,85 @@ int lca(int v, int u) {
 	if (dep[v] > dep[u]) {
 		swap(u, v);
 	}
-	u = getpar(u, dep[u] - dep[v]);
+	u = get_par(u, dep[u] - dep[v]);
 	if (u == v) {
-		return u;
+		return v;
 	}
-	for (int i = 19; i >= 0; --i) {
+	for (int i = LG - 1; i >= 0; --i) {
 		if (par[i][v] != par[i][u]) {
-			u = par[i][u];
 			v = par[i][v];
+			u = par[i][u];
 		}
 	}
 	return par[0][v];
 }
 
-int color[N];
-int mn[N][2];
-int cpar[N];
-int sz[N];
-bool used[N];
-
-void cprep(int v, int p) {
-	sz[v] = 1;
-	for (auto &u : g[v]) {
-		if (u != p && !used[p]) {
-			cprep(u, v);
-			sz[v] += sz[u];
-		}
-	}
+int dist(int v, int u) {
+	int l = lca(v, u);
+	return dep[v] - dep[l] + dep[u] - dep[l];
 }
 
-int centroid(int v, int p, int m) {
-	bool good = true;
-	for (auto &u : g[v]) {
-		if (u != p && !used[u]) {
-			int ret = centroid(u, v, m);
-			if (~ret) {
-				return ret;
-			}
-			if (sz[u] > m / 2) {
-				good = false;
+int d[N];
+
+void bfs() {
+	queue<int> q;
+	memset(d, 0x3f, sizeof(int) * n);
+	for (auto &v : qs) {
+		q.push(v);
+		d[v] = 0;
+	}
+	while (!q.empty()) {
+		int v = q.front();
+		q.pop();
+		for (auto &u : g[v]) {
+			if (d[u] == 0x3f3f3f3f) {
+				d[u] = d[v] + 1;
+				q.push(u);
 			}
 		}
 	}
-	if (m - sz[v] > m / 2) {
-		good = false;
+	for (int i = 0; i < n; ++i) {
+		bst[i] = min(bst[i], d[i]);
 	}
-	return good ? v : -1;
 }
 
-void add(int v) {
-	color[v] = true;
-	int u = v;
-	int tmp = -1;
-	while (u != -1) {
-		assert(tmp != u);
-		int dist = dep[v] + dep[u] - 2 * dep[lca(u, v)];
-		if (dist < mn[u][0]) {
-			mn[u][1] = mn[u][0];
-			mn[u][0] = dist;
-		} else if (mn[u][1] > dist) {
-			mn[u][1] = dist;
-		}
-		tmp = u;
-		u = cpar[u];
-	}
+void recalc() {
+	bfs();
+	qs.clear();
+	memset(mark, 0, sizeof(bool) * n);
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	cin >> n >> que;
+	cin >> n >> m;
 	for (int i = 0; i < n - 1; ++i) {
-		int v, u;
-		cin >> v >> u;
-		--v, --u;
-		g[v].push_back(u);
+		int u, v;
+		cin >> u >> v;
+		--u, --v;
 		g[u].push_back(v);
+		g[v].push_back(u);
 	}
 	
-	queue<pair<int, int>> q;
-	q.push({0, -1});
-	while (!q.empty()) {
-		int v = q.front().first, cp = q.front().second;
-		q.pop();
-		cprep(v, -1);
-		int c = centroid(v, -1, sz[v]);
-		cpar[c] = cp;
-		used[c] = true;
-		for (auto &u : g[c]) {
-			if (!used[u]) {
-				q.push({u, c});
-			}
-		}
-	}
-	
-	prep(0, -1);
-
+	prep(0, 0);
 	for (int i = 0; i < n; ++i) {
-		mn[i][0] = mn[i][1] = 1e9;
+		bst[i] = dist(0, i);
 	}
 
-	add(0);
-
-	while (que--) {
+	while (m--) {
 		int op, v;
 		cin >> op >> v;
 		--v;
 		if (op == 1) {
-			add(v);
+			qs.push_back(v);
+			if ((int) qs.size() == SQ) {
+				recalc();
+			}
 		} else {
-			int u = v;
-			int ans = color[v] ? 0 : (int) 1e9;
-			int tmp = -1;
-			while (u != -1) {
-				assert(u != tmp);
-				int dist = dep[v] + dep[u] - 2 * dep[lca(u, v)];
-				if (color[v] && dist == mn[u][0]) {
-					ans = min(dist + mn[u][1], ans);
-				} else {
-					ans = min(dist + mn[u][0], ans);
-				}
-				tmp = u;
-				u = cpar[u];
+			int ans = bst[v];
+			for (auto &u : qs) {
+				ans = min(ans, dist(u, v));
 			}
 			cout << ans << '\n';
 		}
