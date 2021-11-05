eef6846aa88be3354b40dd3c7521eec6249e9e0e
Ilia Mireskandari <ilia.mireskandari@gmail.com>
2021-11-05 15:15:00 -0400

    Add solution for problem `D - Tree and Queries` Link: https://codeforces.com/contest/375/problem/D Tags: data structures, dfs and similar, trees, *2400 Ref: https://codeforces.com/contest/375/submission/134433512

--- codeforces/375/D.cpp
+++ codeforces/375/D.cpp
@@ -2,25 +2,44 @@
 using namespace std;
 #define cerr cerr << "DEBUG "
 
-constexpr int N = 1e5 + 10;
+constexpr int N = 1e5 + 10, SQ = 400;
 
+int n, m, a[N], c[N], order[N], tin[N], tout[N], timer;
 vector<int> g[N];
-int n, m;
-vector<int> st[N];
-int cnt[N], sz[N], col[N];
-int fen[N + 1];
-vector<pair<int, int>> que[N];
+
+void dfs(int v, int p) {
+	order[timer] = v;
+	tin[v] = timer++;
+	for (auto &u : g[v]) {
+		if (u != p) {
+			dfs(u, v);
+		}
+	}
+	tout[v] = timer;
+}
+
+struct Query {
+	int l, r, t, i;
+
+	bool operator<(const Query &o) const {
+		return r < o.r;
+	}
+};
+
+vector<Query> qs[N];
 int ans[N];
 
-void update(int i, int x) {
-	i = N - 1 - i;
-	for (++i; i < N + 1; i += i & -i) {
+int fen[N], cnt[N];
+
+void upd(int i, int x) {
+	i = N - i - 2;
+	for (++i; i < N; i += i & -i) {
 		fen[i] += x;
 	}
 }
 
 int query(int i) {
-	i = N - 1 - i;
+	i = N - i - 2;
 	int res = 0;
 	for (++i; i > 0; i -= i & -i) {
 		res += fen[i];
@@ -28,98 +47,70 @@ int query(int i) {
 	return res;
 }
 
-void change(int c, int num) {
-	update(cnt[c], -1);
-	cnt[c] += num;
-	update(cnt[c], +1);
-}
-
-void prep(int v, int p) {
-	sz[v] = 1;
-	for (auto &u : g[v]) {
-		if (u != p) {
-			prep(u, v);
-			sz[v] += sz[u];
-		}
-	}
-}
-
-void dfs(int v, int p, bool keep) {
-	sz[v] = 1;
-	int big = -1, bigs = 0;
-	for (auto &u : g[v]) {
-		if (u != p) {
-			sz[v] += sz[u];
-			if (bigs < sz[u]) {
-				big = u;
-				bigs = sz[u];
-			}
-		}
-	}
-
-	if (~big) {
-		for (auto &u : g[v]) {
-			if (u != p && u != big) {
-				dfs(u, v, false);
-			}
-		}
-		dfs(big, v, true);
-		st[v].swap(st[big]);
-		for (auto &u : g[v]) {
-			if (u != p && u != big) {
-				for (auto &color : st[u]) {
-					change(color, +1);
-					st[v].push_back(color);
-				}
-			}
-		}
-	} else {
-		st[v] = vector<int>();
-	}
-	
-	st[v].push_back(col[v]);
-	change(col[v], +1);
-
-	for (auto &qq : que[v]) {
-		ans[qq.second] = query(qq.first);
-	}
-
-	if (!keep) {
-		for (auto &color : st[v]) {
-			change(color, -1);
-		}
-	}
+void update(int i, int x) {
+	upd(cnt[i], -1);
+	cnt[i] += x;
+	upd(cnt[i], 1);
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	for (int i = 0; i < N; ++i) {
-		update(0, +1);
-	}
-
 	cin >> n >> m;
 	for (int i = 0; i < n; ++i) {
-		cin >> col[i];
+		cin >> c[i];
 	}
 	for (int i = 0; i < n - 1; ++i) {
-		int u, v;
-		cin >> u >> v;
-		--u, --v;
-		g[u].push_back(v);
+		int v, u;
+		cin >> v >> u;
+		--v, --u;
 		g[v].push_back(u);
+		g[u].push_back(v);
 	}
-	
+
+	dfs(0, -1);
+
 	for (int i = 0; i < m; ++i) {
 		int v, k;
 		cin >> v >> k;
 		--v;
-		que[v].push_back({k, i});
+		qs[tin[v] / SQ].push_back({tin[v], tout[v], k, i});
 	}
-	
-	prep(0, -1);
-	dfs(0, -1, true);
+		
+	for (int i = 0; i < n; ++i) {
+		if (qs[i].empty()) {
+			continue;
+		}
+		sort(qs[i].begin(), qs[i].end());
+		memset(fen, 0, sizeof(fen));
+		memset(cnt, 0, sizeof(cnt));
+		int ml = qs[i][0].l, mr = qs[i][0].l;
+		for (; mr < qs[i][0].r; ++mr) {
+			update(c[order[mr]], 1);
+		}
+		ans[qs[i][0].i] = query(qs[i][0].t);
+		bool first = true;
+		for (auto &q : qs[i]) {
+			if (first) {
+				first = false;
+				continue;
+			}
+			for (; mr < q.r; ++mr) {
+				update(c[order[mr]], 1);
+			}
+			if (ml < q.l) {
+				for (; ml < q.l; ++ml) {
+					update(c[order[ml]], -1);
+				}
+			} else {
+				for (; ml > q.l; --ml) {
+					update(c[order[ml - 1]], 1);
+				}
+			}
+			ans[q.i] = query(q.t);
+		}
+	}
 
 	for (int i = 0; i < m; ++i) {
 		cout << ans[i] << '\n';
